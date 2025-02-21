b4c55d88f44789c1175f1fe7352fecf00fd45c12
Jeremiamichael <jeremia.mikhael@binus.ac.id>
2025-02-21 19:44:16 +0700

    firts commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jeremiamichael/print-slice
+
+go 1.24.0
---
+++ print_slice.go
@@ -0,0 +1,32 @@
+package print_slice
+
+import (
+	"fmt"
+)
+
+func PrintArray(arr []int) {
+	fmt.Print("[")
+	for i := 0; i < len(agorr); i++ {
+		fmt.Printf("%d", arr[i])
+		if i != len(arr)-1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Print("]")
+}
+func PrintStrArray(arr []string) {
+	fmt.Print("[")
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%s", arr[i])
+		if i != len(arr)-1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Print("]")
+}
+func PrintMatrix(mat [][]int) {
+	for _, row := range mat {
+		PrintArray(row)
+		fmt.Println()
+	}
+}
